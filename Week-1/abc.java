52e46dc005bd78bcbbe64c0e0fc50314a1a2b67f
Nishan Prajapati <nishanprajapati@Sagars-MacBook-Air.local>
2022-11-30 16:47:51 +0545

    add new html file and image file

---
+++ Week-1/abc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SimpleIntrest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the principal (amount), time, and rate::\n");
+
+        float p = in.nextFloat(); // value of the principal
+        float t = in.nextFloat(); // value of the time
+        float r = in.nextFloat(); // value of the rate
+        float SI; // value of the simple interest
+
+        // It will calculate the simple interest
+        SI = (p * t * r) / 100;
+
+        // It will produce the final output
+        System.out.println("\nSimple Interest = " + SI);
+    }
+}
